1c195a1f1360cff8cb47a0dd4eae05e6f0c29e00
Manyata-Patel <manyata.patel11@gmail.com>
2024-01-11 18:40:15 -0600

    MP - Added vision alignment code for testing

--- src/main/java/frc/robot/subsystems/vision/Vision.java
+++ src/main/java/frc/robot/subsystems/vision/Vision.java
@@ -2,16 +2,91 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.vision;
+package frc.robot.subsystems;
 
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonUtils;
+
+import com.revrobotics.CANSparkMax;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.simulation.DifferentialDrivetrainSim;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.VisionConstants;
 
 public class Vision extends SubsystemBase {
-  /** Creates a new Vision. */
-  public Vision() {}
+  /** Creates a new VisionSubsystem. */
+  PhotonCamera camera;
+
+
+  
+  PIDController aimingController;
+  PIDController rangeController;
+
+  XboxController controller;
+
+  // These are temporary and would be replaced with the drive motors
+  // Declared in the drive subsystem
+  private CANSparkMax frontLeft;
+  private CANSparkMax backLeft;
+  private CANSparkMax frontRight;
+  private CANSparkMax backRight;
+
+  double forward;
+  double theta;
+
+  private DifferentialDrive drive = new DifferentialDrive(frontLeft, frontRight);
+  
+  public Vision() {
+    camera = new PhotonCamera("camera");
+    controller = new XboxController(1);
+    backLeft.follow(frontLeft, false);
+    backRight.follow(frontRight, false);
+
+    rangeController = new PIDController(VisionConstants.forwardP, VisionConstants.forwardI, VisionConstants.forwardD);
+    aimingController = new PIDController(VisionConstants.angularP, VisionConstants.angularI, VisionConstants.angularD);
+
+  }
+
+  public void aimAndRange() {
+    if(controller.getAButton()) {
+
+      var result = camera.getLatestResult();
+
+      double targetPitchRadians = Math.toRadians(result.getBestTarget().getPitch());
+
+      if(result.hasTargets()) {
+        
+        double range = 
+        PhotonUtils.calculateDistanceToTargetMeters(VisionConstants.k_cameraHeightMeters, VisionConstants.k_targetHeightMeters, VisionConstants.k_cameraPitchRadians, targetPitchRadians);
+
+        forward = -rangeController.calculate(range, VisionConstants.k_goalRangeMeters);
+        theta = -aimingController.calculate(result.getBestTarget().getYaw(), 0);
+
+        drive.arcadeDrive(forward, theta);
+
+      }
+      else {
+        forward = 0;
+        theta = 0;
+      }
+    }
+
+    else {
+      forward = controller.getLeftY();
+      theta = controller.getRightX();
+
+      drive.arcadeDrive(forward, theta);
+    }
+   
+  
+  }
+  
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    DifferentialDrivetrainSim sim = new DifferentialDrivetrainSim(null, null, forward, theta, forward, null);
   }
 }
--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -1,5 +1,43 @@
-package frc.robot.subsystems.vision;
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-public class VisionConstants {
-    
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
+ * constants. This class should not be used for any other purpose. All constants should be declared
+ * globally (i.e. public static). Do not put anything functional in this class.
+ *
+ * <p>It is advised to statically import this class (or one of its inner classes) wherever the
+ * constants are needed, to reduce verbosity.
+ */
+public final class Constants {
+  public static class OperatorConstants {
+    public static final int kDriverControllerPort = 0;
+  }
+
+  public static class VisionConstants {
+
+    // FIND MEASUREMENTS ONCE CAMERA IS MOUNTED:
+    public static final double k_cameraHeightMeters = 0;
+    public static final double k_targetHeightMeters = 0;
+    public static final double k_cameraPitchRadians = 0;
+
+    // This measurement is if we have the fixed shooter height be made specifically
+    // for shooting into the speaker from a distance and is according to a path
+    public static final double k_goalRangeMeters = Units.inchesToMeters(194.3);
+
+    // Aiming constants - NOT TUNED:
+    public static final double angularP = 0;
+    public static final double angularI = 0;
+    public static final double angularD = 0;
+
+    // Getting in range constants - NOT TUNED:
+    public static final double forwardP = 0;
+    public static final double forwardI = 0;
+    public static final double forwardD = 0;
+  }
 }
