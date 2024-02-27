61a8228d775b89f31843129713a732c51578ef86
ncano1025 <ncano1025@gmail.com>
2024-02-26 19:09:01 -0600

    NC - ShooterConstants.java commit

--- src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterConstants.java
@@ -1,5 +1,11 @@
 package frc.robot.subsystems.shooter;
 
 public class ShooterConstants {
-    
+    public final static int k_leftShootMotorID = 15;
+    public final static int k_rightShootMotorID = 16;
+    public final static int k_smartCurrentLimit = 40;
+    public final static double k_shootSpeaker = 1;
+    public final static double k_shootAmp = 0.5;
+    public final static double k_shootZero = 0.0;
+    public final static boolean k_isInverted = true;
 }
