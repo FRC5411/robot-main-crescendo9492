72fbfc0f9c56144bbab1287a5825d0fd79067fe6
ncano1025 <ncano1025@gmail.com>
2024-02-26 19:08:22 -0600

    NC - Shooter.java commit

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -4,14 +4,50 @@
 
 package frc.robot.subsystems.shooter;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
   /** Creates a new Shooter. */
-  public Shooter() {}
+  private CANSparkMax leftShootMotor;
+  private CANSparkMax rightShootMotor;
+
+  public Shooter() {
+    leftShootMotor = new CANSparkMax(ShooterConstants.k_leftShootMotorID, MotorType.kBrushless);
+    rightShootMotor = new CANSparkMax(ShooterConstants.k_rightShootMotorID, MotorType.kBrushless);
+
+    rightShootMotor.follow(leftShootMotor, !ShooterConstants.k_isInverted);
+    leftShootMotor.setInverted(ShooterConstants.k_isInverted);
+  }
+
+  // Set motor speeds to the speed needed to score Speaker
+  public void shootSpeaker() {
+    leftShootMotor.set(ShooterConstants.k_shootSpeaker);
+  }
+
+  // Set motor speeds to the speed needed to score Amp, if different from Speaker
+  public void shootAmp() {
+    leftShootMotor.set(ShooterConstants.k_shootAmp);
+  }
+
+  // Set motor speeds to zero
+  public void zero() {
+    leftShootMotor.set(ShooterConstants.k_shootZero);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+  }
+
+  // Configure motors
+  public void configure(CANSparkMax motor) {
+    motor.setIdleMode(IdleMode.kBrake);
+    motor.setSmartCurrentLimit(ShooterConstants.k_smartCurrentLimit);
+    motor.burnFlash();
+    motor.clearFaults();
   }
 }
